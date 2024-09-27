5a393bae27e8ca7aafc117735b4cea09f43408f5
Q <nhatquang2106qh@gmail.com>
2024-09-27 20:48:48 +0700

    add git pull

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::any('git-pull', function () {
+    return view('git-pull');
+});
 Route::get('/', function () {
     return view('welcome');
 });
